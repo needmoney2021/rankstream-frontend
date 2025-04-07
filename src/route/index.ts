6468384efd365f8a2a9ea1de8e42ddc64befdc7a
Seunghyun0Shin <shshin@justed.co.kr>
2025-04-07 09:09:22 +0900

    router fix

--- src/route/index.ts
+++ src/route/index.ts
@@ -3,16 +3,6 @@ import landings from './landings/landings'
 
 const routes = [
     ...landings,
-    {
-        path: '/docs',
-        name: 'Docs',
-        component: () => import('../pages/docs/index.vue')
-    },
-    {
-        path: '/signup',
-        name: 'SignUp',
-        component: () => import('../pages/signup/index.vue')
-    }
 ]
 
 const router = createRouter({
--- src/route/landings/landings.ts
+++ src/route/landings/landings.ts
@@ -5,6 +5,16 @@ const routes: RouteRecordRaw[] = [
         path: '/',
         name: 'Landing',
         component: () => import('../../pages/landings/index.vue')
+    },
+    {
+        path: '/docs',
+        name: 'Docs',
+        component: () => import('../../pages/docs/index.vue')
+    },
+    {
+        path: '/signup',
+        name: 'SignUp',
+        component: () => import('../../pages/signup/index.vue')
     }
 ]
 
